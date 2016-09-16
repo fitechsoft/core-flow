4392893aa03db403c95084dc3438eec6d91f1b18
Chun Cao <caochun@gmail.com>
2016-09-16 17:25:32 +0800

    activit tests passed

---
+++ src/test/java/com/fitechsoft/flow/core/HireProcessTest.java
@@ -0,0 +1,125 @@
+package com.fitechsoft.flow.core;
+
+import com.fitechsoft.domain.core.FDUser;
+import com.fitechsoft.repository.core.FDUserRepository;
+import org.activiti.engine.*;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.subethamail.wiser.Wiser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ActivitiConfig.class})
+public class HireProcessTest {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private HistoryService historyService;
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+
+    @Autowired
+    private IdentityService identityService;
+
+    @Autowired
+    private FDUserRepository userRepository;
+
+    private Wiser wiser;
+
+
+    @Before
+    public void setup() {
+
+        String deploymentId = repositoryService.createDeployment()
+                .addClasspathResource("processes/Developer_Hiring_with_jpa.bpmn20.xml")
+                .deploy()
+                .getId();
+
+        Group group = identityService.newGroup("user");
+        group.setName("users");
+        group.setType("security-role");
+        identityService.saveGroup(group);
+
+        User admin = identityService.newUser("admin");
+        admin.setPassword("admin");
+        identityService.saveUser(admin);
+
+        wiser = new Wiser();
+        wiser.setPort(1025);
+        wiser.start();
+    }
+
+    @After
+    public void cleanup() {
+        wiser.stop();
+    }
+
+    @Test
+    public void testHappyPath() {
+
+        // Create test applicant
+        FDUser applicant = new FDUser("John Doe", "john@activiti.org", "12344");
+        userRepository.save(applicant);
+
+        // Start process instance
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("applicant", applicant);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("hireProcessWithJpa", variables);
+
+        // First, the 'phone interview' should be active
+        Task task = taskService.createTaskQuery()
+                .processInstanceId(processInstance.getId())
+                .taskCandidateGroup("dev-managers")
+                .singleResult();
+        Assert.assertEquals("Telephone interview", task.getName());
+
+        // Completing the phone interview with success should trigger two new tasks
+        Map<String, Object> taskVariables = new HashMap<String, Object>();
+        taskVariables.put("telephoneInterviewOutcome", true);
+        taskService.complete(task.getId(), taskVariables);
+
+        List<Task> tasks = taskService.createTaskQuery()
+                .processInstanceId(processInstance.getId())
+                .orderByTaskName().asc()
+                .list();
+        Assert.assertEquals(2, tasks.size());
+        Assert.assertEquals("Financial negotiation", tasks.get(0).getName());
+        Assert.assertEquals("Tech interview", tasks.get(1).getName());
+
+        // Completing both should wrap up the subprocess, send out the 'welcome mail' and end the process instance
+        taskVariables = new HashMap<String, Object>();
+        taskVariables.put("techOk", true);
+        taskService.complete(tasks.get(0).getId(), taskVariables);
+
+        taskVariables = new HashMap<String, Object>();
+        taskVariables.put("financialOk", true);
+        taskService.complete(tasks.get(1).getId(), taskVariables);
+
+        // Verify email
+        Assert.assertEquals(1, wiser.getMessages().size());
+
+        // Verify process completed
+        Assert.assertEquals(1, historyService.createHistoricProcessInstanceQuery().finished().count());
+
+    }
+
+}
